3ef21c50e96d69ccadeb3f641c230d04dcc8e042
Nikki A <nikki.apostolakis@gmail.com>
2019-03-29 22:50:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ let randomNumber = Math.floor(Math.random() * 20)
 
 let randomInteger = Math.floor(randomNumber)
 
-let randomUserID = Math.floor(Math.random() * 20) + 1000000000
+let randomUserID = Math.floor(Math.random() * 20) + 1000000001
